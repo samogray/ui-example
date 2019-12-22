996e0512cb77fb82f4519e8181650899caaf9e11
Oleksandr Samohrai <sam_gray@ukr.net>
2019-12-22 13:49:41 +0200

    range test

--- src/ui/range/index.js
+++ src/ui/range/index.js
@@ -1,22 +1,42 @@
 import React from "react";
-import PropTypes from 'prop-types'
-import Text from '../text'
-import css from './style.module.scss'
+import PropTypes from "prop-types";
+import Text from "../text";
+import css from "./style.module.scss";
 
-const Range = ({value, minLabel = 1, maxLabel = 100, onChange = () => {}}) => {
-  return <div className={css.range}>
-    {minLabel && <Text tagName="span" type="caption" mixClassName={css.range__label}>{minLabel}</Text>}
-      <input value={value} type="range" onChange={onChange} className={css.range__input}/>
-    {maxLabel && <Text tagName="span" type="caption" mixClassName={css.range__label}>{maxLabel}</Text>}
-  </div>
-}
+const Range = ({
+  value,
+  minLabel = 1,
+  maxLabel = 100,
+  onChange = () => {}
+}) => {
+  return (
+    <div className={css.range} data-testid="range-ui">
+      {minLabel && (
+        <Text tagName="span" type="caption" mixClassName={css.range__label}>
+          {minLabel}
+        </Text>
+      )}
+      <input
+        value={value}
+        type="range"
+        onChange={onChange}
+        className={css.range__input}
+        data-testid="range-input-ui"
+      />
+      {maxLabel && (
+        <Text tagName="span" type="caption" mixClassName={css.range__label}>
+          {maxLabel}
+        </Text>
+      )}
+    </div>
+  );
+};
 
 Range.propTypes = {
   minLabel: PropTypes.number,
   maxLabel: PropTypes.number,
   value: PropTypes.number,
-  onChange: PropTypes.func,
-}
+  onChange: PropTypes.func
+};
 
-export default Range
-
+export default Range;
--- src/ui/table/index.js
+++ src/ui/table/index.js
@@ -1,73 +1,88 @@
 import React from "react";
-import classNames from 'classnames'
-import PropTypes from 'prop-types'
+import classNames from "classnames";
+import PropTypes from "prop-types";
 import css from "./style.module.scss";
 
-const Table = ({headers = [], rows = [], rowHeaders = true} )=> {
+const Table = ({ headers = [], rows = [], rowHeaders = true }) => {
   return (
-      <div className={css['table-container']} role="group" data-testid="table-ui">
-        <table className={css.table}>
-          <tbody>
-            <tr className={classNames(css.table__tr, css['table__tr--header'])} data-testid="table-headers-ui">
-              {headers.map((header, index) => (
-                <th
-                  role="columnheader"
-                  scope="col"
-                  key={index}
-                  className={classNames(css.table__th, css['table__th--header'], header.align && css[`table__th--${header.align}`])}
-                >
-                  {header.title ? header.title : header}
-                </th>
-              ))}
-            </tr>
-            {rows.map((row, index) => (
-              <tr key={index} className={css.table__tr} data-testid="table-rows-ui">
-                {row.map((cell, index) =>
-                  rowHeaders && index < 1 ? (
-                    <th
-                      scope="row"
-                      key={index}
-                      className={classNames(
-                        css.table__th,
-                        headers[index].align && css[`table__th--${headers[index].align}`],
-                      )}
-                    >
-                      {cell}
-                    </th>
-                  ) : (
-                    <td
-                      key={index}
-                      className={classNames(
-                        css.table__td,
-                        headers[index] && headers[index].align && css[`table__td--${headers[index].align}`]
-                      )
-                    }
-                    >
-                      {cell}
-                    </td>
-                  )
+    <div className={css["table-container"]} role="group" data-testid="table-ui">
+      <table className={css.table}>
+        <tbody>
+          <tr
+            className={classNames(css.table__tr, css["table__tr--header"])}
+            data-testid="table-headers-ui"
+          >
+            {headers.map((header, index) => (
+              <th
+                role="columnheader"
+                scope="col"
+                key={index}
+                className={classNames(
+                  css.table__th,
+                  css["table__th--header"],
+                  header.align && css[`table__th--${header.align}`]
                 )}
-              </tr>
+              >
+                {header.title ? header.title : header}
+              </th>
             ))}
-          </tbody>
-        </table>
-      </div>
+          </tr>
+          {rows.map((row, index) => (
+            <tr
+              key={index}
+              className={css.table__tr}
+              data-testid="table-rows-ui"
+            >
+              {row.map((cell, index) =>
+                rowHeaders && index < 1 ? (
+                  <th
+                    scope="row"
+                    key={index}
+                    className={classNames(
+                      css.table__th,
+                      headers[index].align &&
+                        css[`table__th--${headers[index].align}`]
+                    )}
+                  >
+                    {cell}
+                  </th>
+                ) : (
+                  <td
+                    key={index}
+                    className={classNames(
+                      css.table__td,
+                      headers[index] &&
+                        headers[index].align &&
+                        css[`table__td--${headers[index].align}`]
+                    )}
+                  >
+                    {cell}
+                  </td>
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
 Table.propTypes = {
   /** array of headers */
-  headers: PropTypes.arrayOf(PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.shape({
-      title: PropTypes.string,
-      align: PropTypes.oneOf(['left', 'right'])
-    })
-  ])),
+  headers: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.shape({
+        title: PropTypes.string,
+        align: PropTypes.oneOf(["left", "right"])
+      })
+    ])
+  ),
   /** array of rows */
   rows: PropTypes.array,
   /** mark header of row */
   rowHeaders: PropTypes.bool
-}
+};
 
 export default Table;
